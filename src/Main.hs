3dabb6d8f0d089d94831a0de7b5a0bcff90e0f11
Hans <hans@hanshoglund.se>
2015-11-23 16:11:25 +0000

    Build with Stack

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,28 @@
 
 module Main where
 
+import Alan ()
+import Options.Applicative
+import Web.Scotty
+
 main = do
   print "alan!"
+  -- serve
+
+  -- start
+  -- add-stage
+  -- send
+
+
+
+-- REST:
+  -- POST stages
+    -- {dependencies:[{name:"...", version: "0.1.0"},...]}
+  -- GET  stages
+  -- GET  stages/:id
+  -- POST performers
+    -- {stage:id, sources:[{path:"...", code:"..."},...]}
+  -- GET performers
+  -- GET performers/:id
+  -- POST performers/:id/messages
+      -- Posts response directly, or need separate request?
